1eb25ea75fe51957ec7d75614debab498eafedc6
italocegatta <italocegatta@gmail.com>
2019-08-10 14:36:15 -0300

    v1

--- _util.R
+++ _util.R
@@ -1,15 +1,16 @@
-set.seed(1014)
-options(digits = 3)
+set.seed(3005)
+options(digits = 7, scipen = 10000)
 
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
   cache = TRUE,
-  out.width = "70%",
-  fig.align = 'center',
-  fig.width = 6,
-  fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+  eval = FALSE,
+  message = FALSE, 
+  warning = FALSE
+  # out.width = "70%",
+  # fig.align = 'center',
+  # fig.width = 6,
+  # fig.asp = 0.618,  # 1 / phi
+  # fig.show = "hold"
 )
-
-options(dplyr.print_min = 6, dplyr.print_max = 6)
